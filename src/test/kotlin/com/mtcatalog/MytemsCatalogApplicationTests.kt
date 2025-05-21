95fb2e98dacb69ce5801440a53be9d97521e9b0d
natanPrs <natan.void@proton.me>
2025-05-21 16:55:25 -0300

    initial config

---
+++ src/test/kotlin/com/mtcatalog/MytemsCatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mtcatalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MytemsCatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
