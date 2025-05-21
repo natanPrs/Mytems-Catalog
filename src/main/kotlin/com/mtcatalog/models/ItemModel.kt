5a36c70a1dc2cccfa7aa14ebb26d63f9eeb04222
natanPrs <natan.void@proton.me>
2025-05-21 16:54:40 -0300

    feat: Item model

---
+++ src/main/kotlin/com/mtcatalog/models/ItemModel.kt
@@ -0,0 +1,33 @@
+package com.mtcatalog.models
+
+import com.mtcatalog.enums.ItemStatus
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "tb_announced_items")
+data class ItemModel(
+
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    val sellerEmail : String,
+
+    val title: String,
+
+    val price: BigDecimal,
+
+    val gameBelong: String,
+
+    @Enumerated(EnumType.STRING)
+    val itemStatus: ItemStatus,
+
+    val publicationDate: LocalDateTime
+)
