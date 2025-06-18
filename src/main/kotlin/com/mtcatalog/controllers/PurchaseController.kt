397771fd5495e47dab51b74b8293af98b2a1c5d6
natanPrs <natan.void@proton.me>
2025-06-18 10:03:39 -0300

    feat: changing the service

--- src/main/kotlin/com/mtcatalog/controllers/PurchaseController.kt
+++ src/main/kotlin/com/mtcatalog/controllers/PurchaseController.kt
@@ -2,16 +2,18 @@ package com.mtcatalog.controllers
 
 import com.mtcatalog.dtos.PurchaseDto
 import com.mtcatalog.services.ItemService
+import com.mtcatalog.services.PurchaseService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
 
 @RestController
-@RequestMapping("/catalog")
-class CatalogController(private val itemService: ItemService) {
+@RequestMapping("/validate-items")
+class PurchaseController(private val purchaseService: PurchaseService) {
 
     @PostMapping("/purchase-validate-item")
-    fun validateItem(@RequestBody purchaseDto: PurchaseDto): Boolean =
-        itemService.validateItem(purchaseDto)
+    fun validateItem(@RequestBody purchaseDto: PurchaseDto): BigDecimal? =
+        purchaseService.validateItem(purchaseDto)
 }
