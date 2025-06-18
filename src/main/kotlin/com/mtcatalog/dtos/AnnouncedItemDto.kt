3e345f022d1cc36d6c6b80c48a30a80c80eca95d
natanPrs <natan.void@proton.me>
2025-06-18 09:59:02 -0300

    feat: add globalItemId

--- src/main/kotlin/com/mtcatalog/dtos/AnnouncedItemDto.kt
+++ src/main/kotlin/com/mtcatalog/dtos/AnnouncedItemDto.kt
@@ -6,6 +6,7 @@ import java.util.UUID
 
 data class AnnouncedItemDto(
     val title: String,
+    val globalItemId: UUID,
     val localStamp: LocalDateTime,
     val sellerEmail: String,
     val amount: BigDecimal,
