64ffd37204d1bf949d4016381d6fbcb906861960
natanPrs <natan.void@proton.me>
2025-06-18 10:01:41 -0300

    chore: add container Factory to listener method

--- src/main/kotlin/com/mtcatalog/services/ItemService.kt
+++ src/main/kotlin/com/mtcatalog/services/ItemService.kt
@@ -4,25 +4,19 @@ import com.mtcatalog.dtos.AnnouncedItemDto
 import com.mtcatalog.dtos.PurchaseDto
 import com.mtcatalog.enums.ItemStatus
 import com.mtcatalog.mappers.toEntity
-import com.mtcatalog.models.ItemModel
 import com.mtcatalog.repositories.ItemRepository
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
-class ItemService(private val itemRepository: ItemRepository) {
+class ItemService(
+    private val itemRepository: ItemRepository) {
 
-    @KafkaListener(topics = ["announceItem"], groupId = "catalog-group")
+    @KafkaListener(topics = ["announceItem"], groupId = "catalog-group", containerFactory = "announceItemKafkaListenerContainerFactory")
     fun announceItem(announcedItemDto: AnnouncedItemDto){
         runCatching {
             val newAnnouncedItem = announcedItemDto.toEntity()
             itemRepository.save(newAnnouncedItem)
         }
-    }
-
-    fun validateItem(purchaseDto: PurchaseDto): Boolean {
-        return itemRepository.findById(purchaseDto.itemId)
-            .map { it.itemStatus == ItemStatus.ANNOUNCED }
-            .orElse(false)
     }
 }
